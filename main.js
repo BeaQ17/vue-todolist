5592729c117f7dac2ea21a927fc753a5d2632d9e
BeaQ17 <beaq235@gmail.com>
2021-05-21 12:00:21 +0200

    lista task predefinite

---
+++ main.js
@@ -0,0 +1,13 @@
+const app = new Vue ({
+    el: "#app",
+    data:{
+        tasks: [
+            "First task",
+            "Second task",
+            "Third task"
+        ]
+    },
+    methods:{
+
+    }
+});
