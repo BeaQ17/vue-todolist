3ef6a46d2cf23706f2ab228faf365505cae3a2c0
BeaQ17 <beaq235@gmail.com>
2021-05-21 12:33:01 +0200

    utente può eliminare task

--- main.js
+++ main.js
@@ -6,11 +6,16 @@ const app = new Vue ({
             "First task",
             "Second task",
             "Third task"
-        ]
+        ],
+        logoImg: "https://www.boolean.careers/images/misc/logo.png"
     },
     methods:{
         addTask(){
             this.tasks.push(this.newTask)
         },
+        deleteTask(index){
+            console.log("Delete task " + index);
+            this.tasks.splice(index, 1)
+        }
     }
 });
