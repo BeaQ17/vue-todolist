5f2765740574a3b38138923ee231d9bf93b5d1f4
BeaQ17 <beaq235@gmail.com>
2021-05-21 17:25:56 +0200

    completed task list + comments

--- main.js
+++ main.js
@@ -7,15 +7,31 @@ const app = new Vue ({
             "Second task",
             "Third task"
         ],
-        logoImg: "https://www.boolean.careers/images/misc/logo.png"
+        logoImg: "https://www.boolean.careers/images/misc/logo.png",
+        completed: [
+
+        ]
     },
     methods:{
+        /*
+        addTask(){
+            if (task.length > 0) {
+                this.tasks.push(this.newTask)
+                this.newTask=""
+            } else {
+                alert("Please write out the task")
+            }*/
+            
         addTask(){
             this.tasks.push(this.newTask)
             this.newTask=""
         },
         deleteTask(index){
             console.log("Delete task " + index);
+            this.tasks.splice(index, 1)
+        },
+        doneTask(index, task){
+            this.completed.unshift(task)
             this.tasks.splice(index, 1)
         }
     }
